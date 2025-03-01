a8506f2bd267939c0209fc09ae07c8e9d3c09a37
Anshu Anand <anshu.anand@myob.com>
2025-03-01 15:16:06 +1100

    Implement data-driven testing for login validation

--- cypress/e2e/sauceDemo/loginPage.cy.js
+++ cypress/e2e/sauceDemo/loginPage.cy.js
@@ -1,41 +1,36 @@
 import LoginPage from "../../pageObjects/LoginPage";
-import loginTestData from '../../fixtures/loginTestData.json';
+import loginTestData from "../../fixtures/loginTestData.json";
 import InventoryPage from "../../pageObjects/InventoryPage";
 
 const loginPage = new LoginPage();
-const inventoryPage = new InventoryPage();
+//const inventoryPage = new InventoryPage();
 
 describe("Login page", () => {
   beforeEach(() => {
     loginPage.visitLoginPage();
-    })
-  
+  });
+
   it("should load the login page", () => {
     loginPage.isLoginButtonVisible();
   });
 
-  it("should login with valid credentials", () => {
-    const username = loginTestData.validUsername;
-    const password = loginTestData.validPassword;
-    loginPage.validateLogin(username,password,false);
+  loginTestData.validUsername.forEach((username) => {
+    it(`should login with valid username: "${username}" and password`, () => {
+      //const password = loginTestData.validPassword.password;
+      const password = loginTestData.validPassword;
+      loginPage.validateLogin(username, password, false);
+    });
   });
-
-  it("should not login with invalid username", () => {
-    const username = loginTestData.invalidUsername;
-    const password = loginTestData.validPassword;
-    loginPage.validateLogin(username,password,true);
-  });
-
-  it("should not login with invalid password", () => {
-    const username = loginTestData.validUsername;
-    const password = loginTestData.invalidPassword;
-    loginPage.validateLogin(username,password,true);
-  })
 
   it("should not login with invalid username and password", () => {
     const username = loginTestData.invalidUsername;
     const password = loginTestData.invalidPassword;
+    loginPage.validateLogin(username, password, true);
+  });
+
+  it.only("should show error for a locked_out user",() => {
+    const username = loginTestData.lockedOutUser;
+    const password = loginTestData.invalidPassword;
     loginPage.validateLogin(username,password,true);
   })
-  
 });
