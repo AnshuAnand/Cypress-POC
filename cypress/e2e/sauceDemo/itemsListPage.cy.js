23fd6d2c75e0001f790adf97ff26c241cf27c0bc
Anshu Anand <anshu.anand@myob.com>
2025-03-04 07:57:37 +1100

    Create a basic test for Items list page

--- cypress/e2e/sauceDemo/itemsListPage.cy.js
+++ cypress/e2e/sauceDemo/itemsListPage.cy.js
@@ -1,2 +1,18 @@
 import LoginPage from "../../pageObjects/LoginPage";
+import loginTestData from "../../fixtures/loginTestData.json";
 import ItemsListPage from "../../pageObjects/ItemsListPage";
+
+const loginPage = new LoginPage();
+const itemsListPage = new ItemsListPage();
+
+describe("Items list page", () => {
+  beforeEach(() => {
+    
+    loginPage.visitLoginPage();
+    loginPage.validateLogin(loginTestData.validUsername[0],loginTestData.validPassword,false);
+  });
+
+  it("should load the Items list page", () => {
+    itemsListPage.isItemsListVisible();
+  });
+});
