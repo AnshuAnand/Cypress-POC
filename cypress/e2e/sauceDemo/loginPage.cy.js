0b04cd81492c4b3bdf7b0ec0cedffccdd4daf866
Anshu Anand <anshu.anand@myob.com>
2025-03-13 14:16:58 +1100

    fix(fixtures) Correct the usage of fixtures by using cy.fixture command instead of importing the fixture file

--- cypress/e2e/sauceDemo/loginPage.cy.js
+++ cypress/e2e/sauceDemo/loginPage.cy.js
@@ -1,37 +1,41 @@
 /// <reference types="cypress" />
 import LoginPage from "../../pageObjects/LoginPage";
-import loginTestData from "../../fixtures/loginTestData.json";
 
 const loginPage = new LoginPage();
 
 describe("Login page", () => {
+  let loginTestData;
   beforeEach(() => {
+    cy.fixture('loginTestData').then((data) => {
+      loginTestData = data;
+    })
     loginPage.visitLoginPage();
-    
   });
 
   it("should load the login page", () => {
     loginPage.isLoginButtonVisible();
   });
 
-  loginTestData.validUsername.forEach((username) => {
+  it("should not login with invalid username and password", () => {
+    const username = loginTestData.invalidUsername;
+    const password = loginTestData.invalidPassword;
+    const errorMessage =
+      "Epic sadface: Username and password do not match any user in this service";
+    loginPage.validateLogin(username, password, true, errorMessage);
+  });
+
+  /* loginTestData.validUsername.forEach((username) => {
     it(`should login with valid username: "${username}" and password`, () => {
       const password = loginTestData.validPassword;
       loginPage.validateLogin(username, password, false);
     });
-  });
+  }); */
 
-  it("should not login with invalid username and password", () => {
-    const username = loginTestData.invalidUsername;
-    const password = loginTestData.invalidPassword;
-    const errorMessage = "Epic sadface: Username and password do not match any user in this service";
-    loginPage.validateLogin(username, password, true, errorMessage);
-  });
-
-  it("should show error for a locked_out user",() => {
+  it("should show error for a locked_out user", () => {
     const username = loginTestData.lockedOutUser;
     const password = loginTestData.validPassword;
-    const lockedOutErrorMessage = "Epic sadface: Sorry, this user has been locked out.";
-    loginPage.validateLogin(username,password,true, lockedOutErrorMessage);
-  })
+    const lockedOutErrorMessage =
+      "Epic sadface: Sorry, this user has been locked out.";
+    loginPage.validateLogin(username, password, true, lockedOutErrorMessage);
+  });
 });
