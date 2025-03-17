3bd7fe6feb6afe20691c6c253c995ce278ce7f50
Anshu Anand <anshu.anand@myob.com>
2025-03-18 10:13:24 +1100

    (fix) Correct the usage of fixture on Item List page

--- cypress/e2e/sauceDemo/itemsListPage.cy.js
+++ cypress/e2e/sauceDemo/itemsListPage.cy.js
@@ -1,18 +1,20 @@
 import LoginPage from "../../pageObjects/LoginPage";
-import loginTestData from "../../fixtures/loginTestData.json";
 import ItemsListPage from "../../pageObjects/ItemsListPage";
 
 const loginPage = new LoginPage();
 const itemsListPage = new ItemsListPage();
 
 describe("Items list page", () => {
+  let loginTestData;
   beforeEach(() => {
-    
-    loginPage.visitLoginPage();
-    loginPage.validateLogin(loginTestData.validUsername[0],loginTestData.validPassword,false);
+    cy.fixture("loginTestData").then((data) => {
+      loginTestData = data;
+    });
   });
 
   it("should load the Items list page", () => {
+    loginPage.visitLoginPage();
+    loginPage.validateLogin(loginTestData.validUsername[0],loginTestData.validPassword,false);
     itemsListPage.isItemsListVisible();
   });
 });
